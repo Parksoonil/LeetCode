90ef666d880e3d9a28cb44cefbe1432ec6e16af2
Parksoonil <100554960+Parksoonil@users.noreply.github.com>
2022-04-11 15:24:50 +0900

    Time: 100 ms (84.87%), Space: 14.2 MB (71.82%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,15 +1,26 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        nums = []
-        result = 0.0
-        while nums1 or nums2:
-            if nums1:
-                nums.append(nums1.pop())
-            if nums2:
-                nums.append(nums2.pop())
-        nums.sort()
-        if len(nums) % 2 == 1:
-            result = nums[len(nums)//2]
+        l = len(nums1) + len(nums2)
+        if l % 2 == 1:
+            return self.kth(nums1, nums2, l // 2)
+        else:   
+            return (self.kth(nums1, nums2, l // 2) + self.kth(nums1, nums2, l // 2 - 1)) / 2
+    
+    def kth(self, a, b, k):
+        if not a:
+            return b[k]
+        if not b:
+            return a[k]
+        ia, ib = len(a) // 2, len(b) // 2
+        ma, mb = a[ia], b[ib]
+        
+        if ia + ib < k:
+            if ma > mb:
+                return self.kth(a, b[ib + 1:], k - ib - 1)
+            else:
+                return self.kth(a[ia + 1:], b, k - ia - 1)
         else:
-            result = (nums[len(nums)//2 - 1] + nums[len(nums)//2])/2
-        return result
+            if ma > mb:
+                return self.kth(a[:ia], b, k)
+            else:
+                return self.kth(a, b[:ib], k)

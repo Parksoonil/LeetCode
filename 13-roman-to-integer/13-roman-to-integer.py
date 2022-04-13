9a00ade38f5748c3efd0ea1953ed98ad1abf237d
Parksoonil <100554960+Parksoonil@users.noreply.github.com>
2022-04-13 14:51:23 +0900

    Time: 54 ms (77.12%), Space: 13.8 MB (98.74%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,39 +1,12 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        result = 0
-        strlist = list(s)
-        for i, v in enumerate(strlist):
-            if v == "M":
-                if i != 0 and strlist[i - 1] == "C":
-                    result += 800
-                else:
-                    result += 1000
-            elif v == "D":
-                if i != 0 and strlist[i - 1] == "C":
-                    result += 300
-                else:
-                    result += 500
-            elif v == "C":
-                if i != 0 and strlist[i - 1] == "X":
-                    result += 80
-                else:
-                    result += 100
-            elif v == "L":
-                if i != 0 and strlist[i - 1] == "X":
-                    result += 30
-                else:
-                    result += 50
-            elif v == "X":
-                if i != 0 and strlist[i - 1] == "I":
-                    result += 8
-                else:
-                    result += 10
-            elif v == "V":
-                if i != 0 and strlist[i - 1] == "I":
-                    result += 3
-                else:
-                    result += 5
-            elif v == "I":
-                result += 1
+        result, prev = 0, 0
+        dict = { "I":1, "V":5, "X":10, "L":50,
+                 "C":100, "D":500, "M":1000}
+        for i in s[::-1]:
+            if dict[i] >= prev:
+                result += dict[i]
+            else:
+                result -= dict[i]
+            prev = dict[i]
         return result
-            

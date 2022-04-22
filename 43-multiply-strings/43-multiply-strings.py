bc30e39b8c48b502d2451b5a58c0ce9ad6434245
Parksoonil <100554960+Parksoonil@users.noreply.github.com>
2022-04-22 18:04:23 +0900

    Time: 178 ms (36.87%), Space: 13.9 MB (75.79%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.py
+++ 43-multiply-strings/43-multiply-strings.py
@@ -1,11 +1,15 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        dict = {"0": 0,"1":1 ,"2":2 ,"3": 3,"4": 4,"5":5 ,"6": 6,"7": 7,"8":8,"9":9 }
-        xlen = len(num1)
-        ylen = len(num2)
-        x =y =0
-        for i,n in enumerate(num1):
-            x += (10**(xlen - (i+1)) * dict[n])
-        for i,n in enumerate(num2):
-            y += (10**(ylen - (i+1)) * dict[n])
-        return str(x * y)
+        n, m = len(num1), len(num2)
+        if not n or not m:
+            return '0'
+        res = [0] * (n + m)
+        for i in reversed(range(n)):
+            carry = 0
+            for j in reversed(range(m)):
+                tmp = (ord(num1[i])-ord('0'))*(ord(num2[j])-ord('0')) + carry
+                carry = (res[i+j+1]+tmp) // 10
+                res[i+j+1] = (res[i+j+1]+tmp) % 10
+            res[i] += carry
+        res = ''.join(map(str, res))
+        return '0' if not res.lstrip('0') else res.lstrip('0')

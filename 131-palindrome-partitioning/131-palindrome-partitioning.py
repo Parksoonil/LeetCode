c71292df690bfb261de5076a92910b97ab96ac0a
Parksoonil <100554960+Parksoonil@users.noreply.github.com>
2022-05-19 13:21:05 +0900

    Time: 1308 ms (5.83%), Space: 29 MB (87.09%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -0,0 +1,16 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        result = []
+        self.dfs(s, [], result)
+        return result
+    
+    def dfs(self, s, path, result):
+        if not s:
+            result.append(path)
+            return
+        for i in range(1, len(s) + 1):
+            if self.isPal(s[:i]):
+                self.dfs(s[i:], path + [s[:i]], result)
+    
+    def isPal(self, s):
+        return s == s[::-1]
